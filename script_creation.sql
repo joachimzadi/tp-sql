9d4b6507e4abc30f891060b8f723034396ea9c56
J4LPROJECTS <gitprojects@j4ltechnologies.com>
2020-07-02 10:43:22 +0200

    Commit Maj creation

--- script_creation.sql
+++ script_creation.sql
@@ -9,7 +9,7 @@ use mocodo_db;
 # Creation de la table Client
 create table if not exists Client
 (
-    idClient int unsigned auto_increment,
+    idClient int unsigned,
     nom      varchar(20) not null,
     email    varchar(50) not null,
     mdp      varchar(15),
@@ -17,6 +17,32 @@ create table if not exists Client
     constraint pk_client primary key (idClient),
     constraint uq_client_email unique (email)
 );
+
+# Alter pour modifier un Objet
+alter table Client
+    alter sexe set default 'F';
+
+
+alter table Client
+    add age int not null;
+
+alter table Client
+    modify ddn YEAR;
+
+insert into client
+values (1, 'Roxana', 'roxana@dawm.ge', '123456', 'F');
+
+insert into client
+values (2, 'Miri', 'miri@dawm.ge', '123456', 'F');
+
+insert into client(idClient, nom, email)
+values (3, 'Toulepi', 'toulepi@dawm.ge');
+
+insert into client(nom, idClient, email)
+values ('Souly', 4, 'souly@dawm.ge');
+
+insert into client(nom, idClient, email)
+values ('Sabrine', 5, 'sabrine@dawm.ge');
 
 create table if not exists Commande
 (
